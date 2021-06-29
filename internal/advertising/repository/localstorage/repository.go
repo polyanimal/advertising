9a010719e3721846546fd4c64d3461b32741a362
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-06-29 19:38:02 +0300

    Implement repository interface

--- internal/advertising/repository/localstorage/repository.go
+++ internal/advertising/repository/localstorage/repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type AdvertisingRepo struct {
-	mu sync.Mutex
+	mu             sync.Mutex
 	advertisements map[int]models.Advertisement
 }
 
@@ -15,4 +15,16 @@ func NewAdvertisingRepo() *AdvertisingRepo {
 	return &AdvertisingRepo{
 		advertisements: m,
 	}
+}
+
+func (r *AdvertisingRepo) GetAllAdvertisements() ([]models.Advertisement, error) {
+	return nil, nil
+}
+
+func (r *AdvertisingRepo) GetAdvertisement() (models.Advertisement, error) {
+	return models.Advertisement{}, nil
+}
+
+func (r *AdvertisingRepo) CreateAdvertisement() error {
+	return nil
 }
