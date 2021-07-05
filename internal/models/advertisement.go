aa2fadaa499742dd87e3187959e99a76bc0d48e3
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-07-05 19:58:37 +0300

    Remove publisher from advertisement model

--- internal/models/advertisement.go
+++ internal/models/advertisement.go
@@ -5,7 +5,6 @@ import "time"
 type Advertisement struct {
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	Publisher   string    `json:"publisher"`
 	PhotoLinks  []string  `json:"photo_links"`
 	Price       uint      `json:"price"`
 	DateCreate  time.Time `json:"date_create"`
